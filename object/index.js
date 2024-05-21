e7875250ae849ac1c7a60c868b08e6038b08e0d2
sharoon kp <sharoonkp267@gmail.com>
2024-05-21 10:42:23 +0530

    feat:iterator on some

--- object/index.js
+++ object/index.js
@@ -1,9 +1,16 @@
 const obj1 = {
-  name:"sharoon",
-  age:21,
-  place:"calicut"
+  name: "sharoon",
+  age: 21,
+  place: "calicut"
 }
 
-console.log(Object.keys(obj1)); // return an array of key's values
+console.log(Object.keys(obj1)); // return an array of key names
 
 console.log(Object.values(obj1)); // return an array of key's values
+
+console.log(Object.entries(obj1)); // return an array of array contains key and value
+
+console.log(Object.assign(obj1, { "cgpa": "2.56" })); // return an object with new value and also modifies the original object
+
+Object.values(obj1).some((data) => console.log(data === "sharoon", "data mack")); //iterates over the values
+
